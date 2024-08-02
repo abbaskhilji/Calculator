f54ffc326cf919bff5b48f6cb05cdd03030afe3c
MARYAM KHALIL <167639658+abbaskhilji@users.noreply.github.com>
2024-08-02 17:17:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// var string ='';
+// var buttons = document.querySelectorAll('.button');
+// Array.from(buttons).forEach((button)=>{
+//    button.addEventListener('click',(e)=>{
+//     if (e.target.innerHTML == '=') {
+//         string = eval(string);
+//         document.querySelector('input').value = string;
+
+        
+//     }
+//     else{
+//     console.log(e.target)
+//     string =string +e.target.innerHTML;
+//     document.querySelector('input').value = string;
+//     }
+
+//    })
+
+// })
+// function light() {
+//     document.getElementById('maryam').value = 1
+//     document.getElementById('button').value = 2
+//     document.getElementById('button1').value = 3
+//     document.getElementById('button3').value = 4
+//     document.getElementById('button4').value =5
+//     document.getElementById('button5').value = 6
+//     document.getElementById('button6').value = 7
+//     document.getElementById('button7').value = 8
+//     if ( document.getElementById('=').value = '=') {
+
+        
+        
+//     }
+
+    
+// }
